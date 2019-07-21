0c14766e99710c5e11a822853f65fec7cb5a2af9
Henry S. Sjøen <hsjo@equinor.com>
2019-07-21 20:51:36 +0200

    update version string

--- lib/helpers/appInfo_helper.dart
+++ lib/helpers/appInfo_helper.dart
@@ -2,11 +2,11 @@ abstract class AppInfo {
   //Todo: Do it programmatically
   // Check out ==> https://pub.dev/packages/package_info
   static String appName = "Åpen Himmel";
-  static String version = "2.0.0";
+  static String version = "2.2.0";
   static String developers = "kodekameratene";
 
   static String festivalId = "SxS6fKd88fpRhwrqWKXo";
-  
+
   static String get dbCollectionContent => 'festival/$festivalId/content';
 
   static String get dbCollectionSponsor => 'festival/$festivalId/sponsors';
